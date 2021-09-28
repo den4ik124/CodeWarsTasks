9cc8e358e422e91a581f1d65518a169ebc457cfe
den4ik124 <den4ik124@gmail.com>
2021-09-28 18:59:28 +0300

    Добавлена ссылка на задачу

--- PINCodeCombinations/Program.cs
+++ PINCodeCombinations/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 namespace CodeWarsTasks
 {
+    //https://www.codewars.com/kata/5263c6999e0f40dee200059d/train/csharp
     internal class Program
     {
         private static void Main(string[] args)
